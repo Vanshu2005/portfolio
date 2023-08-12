efd07b0c6e365c36c696cc05bf2228c81667c536
Vanshika <vanshikaagrawal309@gmail.com>
2023-08-13 00:07:56 +0530

    semi final page

--- script.js
+++ script.js
@@ -32,3 +32,50 @@ let changeText=()=>{
 };
 changeText();
 setInterval(changeText,3000)
+
+// circle skills
+const circles=document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+    var dots = elem.getAttribute("data-dots");
+    var marked=elem.getAttribute("data-percent");
+    var percent=Math.floor(dots*marked/100);
+    var points="";
+    var rotate = 360/dots;
+
+    for(let i=0;i<dots;i++){
+        points+=`<div class="points" style="--i:${1};--rot:${rotate}deg"></div>`;
+    }
+    elem.innerHTML=points;
+    const pointsMarked=elem.querySelectorAll('.points');
+    for(let i=0; i<percent; i++){
+        pointsMarked[i].classList.add('marked')
+    }
+})
+
+
+// mix it up achivement section
+var mixer=mixitup('.portfolio-gallery');
+
+
+// active-menu
+let menuLi=document.querySelectorAll('header ul li a');
+let section=document.querySelectorAll('section');
+
+function activeMenu(){
+    let len=section.length;
+    while(--len && window.scrollY + 97<section[len].offsetTop){}
+    menuLi.forEach(sec=>sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
+
+// sticky-navbar
+
+const header=document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",this.window.scrollY>50)
+}
+)
+
